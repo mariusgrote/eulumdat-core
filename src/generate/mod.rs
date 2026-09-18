fb9c3bf2b0ae4213d82a29c2182fdd6c9e440ed3
Marius <40433434+mariusgrote@users.noreply.github.com>
2026-09-18 12:06:42 +0200

    Add focused polar diagram presentation (#4)
    * feat(svg): add focused polar diagram presentation
    
    * refine focused polar labels and legend

--- src/generate/mod.rs
+++ src/generate/mod.rs
@@ -12,7 +12,7 @@ mod report_svg;
 #[cfg(feature = "generate-png")]
 pub use png::{RasterBackground, RasterOptions};
 #[cfg(feature = "generate-svg")]
-pub use polar_svg::{IntensityMode, PlanePair, PolarDiagramOptions};
+pub use polar_svg::{IntensityMode, PlanePair, PolarDiagramOptions, PolarDiagramPresentation};
 #[cfg(not(feature = "generate-png"))]
 pub use report_svg::{RasterBackground, RasterOptions};
 #[cfg(feature = "generate-svg")]
